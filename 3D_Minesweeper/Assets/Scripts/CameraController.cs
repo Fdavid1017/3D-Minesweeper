32483c88a6387c3a658d18cf10fbb0aa905f70b9
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2019-10-19 18:24:19 +0200

    Bird ambient sound
    Added bird ambient sound to the game scene

---
+++ 3D_Minesweeper/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float time = UnityEngine.Random.Range(0, 5);
+        Invoke("playBirdSounds", time);
+    }
+
+    private void playBirdSounds()
+    {
+
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+
+        float time = UnityEngine.Random.Range(0, 5);
+        Invoke("playBirdSounds", time);
+    }
+}
